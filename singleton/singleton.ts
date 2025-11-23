ee4331c8a1099f46fcfb0f2a9faaedf495cc23f0
Gabriel Soares <gabrielsoacc@gmail.com>
2025-11-23 19:36:36 -0300

    feat: created singleton class

--- singleton/singleton.ts
+++ singleton/singleton.ts
@@ -1,1 +1,17 @@
+export class Singleton {
+  private static instance: Singleton | null = null;
 
+  private constructor() {}
+
+  public static getInstance() {
+    if (this.instance === null) {
+      this.instance = new Singleton();
+      return this.instance;
+    }
+    return this.instance;
+  }
+
+  public logger() {
+    console.log(`Yes, I'm instance of singleton`);
+  }
+}
